e4653089e2b3f495989c824051e9e651af669a8a
raj_viradiya_coder <rajviradiya2019@gmial.com>
2024-01-31 17:08:22 +0530

    Local Storage

--- Main.js
+++ Main.js
@@ -2,87 +2,99 @@ $(document).ready(function () {
    loadDataFromLocal();
 
    $(".todo-inpput").on("click", "button", function () {
-      addDataToLocal($(this));
+      if ($("#txtId").val() == "") {
+         addDataToLocal($(this));
+         location.reload(true);
+      } else {
+         updateDataFromLocal();
+      }
    });
-   
+
    /**Switch Function */
    $(".todo-li").each(function () {
       $(this).on("click", "#custom-toggle", function () {
-         switchElement($(this))
+         switchElement($(this));
       });
    });
 
    /**Edit element*/
    $(".todo-li").on("click", ".editBtn", function () {
-      const title = $(this).parent(".f-button").prev(".lable1").find("label").html()
-      const id = $(this).parents("li").find("")
-      console.log(id)
+      const title = $(this).parent(".f-button").prev(".lable1").find("label").html();
+      const id = $(this).parents("li").find("input").attr("data-id");
 
-      $("#input1").val(title);   
+      $("#input1").val(title);
       $("#txtId").val(id);
-      $(".todo-inpput").find("button").text("save") 
-
-
-      
-      console.log()
-      /*editElement($(this));*/
+      $(".todo-inpput").find("button").html(`<i class="fa-solid fa-file-export"></i>`);
    });
-
 
    /*delete*/
    $(".todo-li").on("click", ".deleteBtn", function () {
-      $(this).closest("li").remove();
+      const id = $(this).parents("li").attr("id")
+      const localArray = JSON.parse(localStorage.getItem("localData"));
+
+      localArray.splice(id > 0 ? 0 : id - 1, 1);
+
+      localStorage.setItem("localData", JSON.stringify(localArray));
+      location.reload()
    });
+
    /*clear All*/
    $(".clear").on("click", function () {
-      $(".todo-li").empty();
+      const clearall = $(this).parents(".todo-body").find(".todo-li").empty();
+      localStorage.clear();
+      loadDataFromLocal();
    });
 
    /*filter*/
-   
+   $(".btn1").on("click", function () {
+      All()
+   })
+   $(".btn2").on("click", function () {
+      Active()
+   })
+   $(".btn3").on("click", function () {
+      Complited()
+   })
 });
 
+/*load html data */
 function loadDataFromLocal() {
    let localdata = localStorage.getItem("localData");
+   let localArray = JSON.parse(localdata);
 
    if (localdata !== null && localdata !== undefined) {
-      let localArray = JSON.parse(localdata);
       localArray.forEach((element) => {
          let dynamicList = `<li class ="" id=${element.id}>`;
-         dynamicList = dynamicList + `<div class="wrapper">`;
-         dynamicList = dynamicList + `<input type="range" name="points" data-id=${element.id} min="1" step="1" id="custom-toggle" class="tgl-on " max="3" value="${element.state}">`;
-         dynamicList = dynamicList + `</div>`;
-         dynamicList = dynamicList + `<div class="lable1">`
-         dynamicList = dynamicList + `<label class="titleLable overflow-hidden">${element.title}</label>`;
-         dynamicList = dynamicList + `</div>`
-         dynamicList = dynamicList + `<div class= "f-button">`;
-         dynamicList = dynamicList + `<button class="editBtn"><i class="fa-solid fa-pen-to-square"></i></button>`;
-         dynamicList = dynamicList + `<button class="deleteBtn"><i class="fa-solid fa-trash"></i></button>`;
-         dynamicList = dynamicList + `</div>`;
-         dynamicList = dynamicList + `</li>`;
+         dynamicList += `<div class="wrapper">`;
+         dynamicList += `<input type="range" name="points" data-id=${element.id} min="1" step="1" id="custom-toggle" class="${element.color}" max="3" value="${element.state}">`;
+         dynamicList += `</div>`;
+         dynamicList += `<div class="lable1">`;
+         dynamicList += `<label class="titleLable overflow-hidden">${element.title}</label>`;
+         dynamicList += `</div>`;
+         dynamicList += `</div>`;
+         dynamicList += `<div class= "f-button">`;
+         dynamicList += `<button class="editBtn"><i class="fa-solid fa-pen-to-square"></i></button>`;
+         dynamicList += `<button class="deleteBtn"><i class="fa-solid fa-trash"></i></button>`;
+         dynamicList += `</div>`;
+         dynamicList += `</li>`;
          $(".todo-li").append(dynamicList);
-         
-      });
+      })
    }
 }
 
+/*add data to local storage*/
 function addDataToLocal(input1) {
    let localData = localStorage.getItem("localData");
    let input = $(".todo-inpput").find("input");
    let inputval = input.val().trim();
-   let color = $(".tgl-on")
 
-   let state = input1.parents(".todo-inpput").next(".todo-body").find("li").find("#custom-toggle").val()
-   console.log(state,"state of")
-
-   console.log("color",color)
    if (localData) {
       let localArray = JSON.parse(localData);
       if (inputval !== "") {
          const obj = {
             id: localArray.length + 1,
-            state: state,
-            color: color,
+            state: "1",
+            color: "tgl-on",
             title: inputval,
          };
          localArray.push(obj);
@@ -94,8 +106,8 @@ function addDataToLocal(input1) {
          let arryObj = [];
          let obj = {
             id: 1,
-            state: state,
-            color:color,
+            state: "1",
+            color:"tgl-on",
             title: inputval,
          };
          arryObj.push(obj);
@@ -108,107 +120,127 @@ function addDataToLocal(input1) {
 /**switch function */
 let state = 1;
 
-function switchElement(element) {
-   console.log("state11", state)
-
-   value = parseInt(element.val(), 10);
-   console.log("value", value)
-
+function switchElement(element) { //#custom-toggle switch = element
+   let localData = localStorage.getItem("localData");
+   let localArray = JSON.parse(localData);
+   let value = parseInt(element.val(), 10);
    if (value === 1 && state === 1) {
-      state = 2
+      state = 2;
       element.val(state);
    } else if (value === 2 && state === 2) {
-      state = 3
+      state = 3;
       element.val(state);
-   } else if (state === 3 && value === 3) {
-      state = 1
+   } else if (value === 3 && state === 3) {
+      state = 1;
       element.val(state);
-   } else if (state === 3 && value === 2) {
-      state = 3
+   } else if (value === 2 && state === 3) {
+      state = 3;
       element.val(state);
-   } else if (state === 2 && value === 1) {
-      state = 2
+   } else if (value === 1 && state === 2) {
+      state = 2;
       element.val(state);
-   } else {
-      state = 1
+   } else if(value === 2 && state === 1){
+      state = 3;
+      element.val(state);
+   }else {
+      state = 1;
       element.val(state);
    }
 
    if (state === 1) {
-      element.removeClass("tgl-off tgl-def").addClass("tgl-on")
+      element.removeClass("tgl-off tgl-def").addClass("tgl-on");
    } else if (state === 2) {
-      element.removeClass("tgl-on tgl-off").addClass("tgl-def")
+      element.removeClass("tgl-on tgl-off").addClass("tgl-def");
    } else if (state === 3) {
-      element.removeClass("tgl-def tgl-on").addClass("tgl-off")
+      element.removeClass("tgl-def tgl-on").addClass("tgl-off");
    }
 
-   let localData = localStorage.getItem("localData");
-   let localArray = JSON.parse(localData);
-
- localArray.forEach(data => {
-   if(data.id == element.attr("data-id")){
-      data.state = element.val()
-      data.color = element.attr()
-      console.log(data.state)
-   }
-   
-   localStorage.setItem("localData", JSON.stringify(localArray));
-   console.log(localArray)
-})
+   localArray.find((data) => {
+      if (data.id == element.attr("data-id")) {
+         data.state = element.val();
+         data.color = element.attr("class")
+      }
+      localStorage.setItem("localData", JSON.stringify(localArray));
+   });
 }
 
 /**Edit Fuctionality */
-
-
-function editElement(element) {
-   let listItem = $(element).closest("li");
-   let label = listItem.find(".form-check-label");
-   let value = label.text();
-   let inputedit = $(".todo-inpput").closest("input");
-   let buttonedit = $(".todo-inpput").find("button");
-
+function updateDataFromLocal() {
    let localData = localStorage.getItem("localData");
    let localArray = JSON.parse(localData);
 
-   console.log(localArray)
-   console.log($("#textId").val())
-
-   localArray.find(data => console.log(data.id))
-
-
+   const oldRecord = localArray.find((m) => m.id == $("#txtId").val());
+   oldRecord.title = $(".todo-inpput").find("#input1").val();
+   $(".todo-inpput").find("button").html(`<i class="fa-solid fa-plus"></i>`);
+   $("totdo-inpput").find("#input1").val("");
+   console.log(oldRecord)
    localStorage.setItem("localData", JSON.stringify(localArray));
-   loadDataFromLocal()
-
-   inputedit.replaceWith(`<input type="text" value="${value}">`);
-   buttonedit.replaceWith(
-      `<button type="button" class=" btn btn-primary"><i class="fa-solid fa-file-export"></i></button>`
-   );
-
-   $(".todo-inpput")
-      .find("button")
-      .on("click", function () {
-         saveElement(label);
-      });
+   location.reload(true);
 }
+//Filter
+// All list
+function All() {
+   let val = $(".todo-li").empty()
+   console.log(val,"val")
+         loadDataFromLocal()
+   }
+//Pending list
+function Active() {
+   let val = $(".todo-li").empty()
+   console.log(val,"val")
 
-function saveElement(label) {
-   let editedValueli = $(".todo-inpput").find("input");
-   let editedValueli2 = editedValueli.val();
-   let editbutton = $(".todo-inpput").find("button");
+   let localdata = localStorage.getItem("localData");
+   let localArray = JSON.parse(localdata);
 
-   label.replaceWith(
-      `<label class="titleLableoverflow-hidden " for="flexSwitchCheckChecked">${editedValueli2}</label>`
-   );
-   editbutton.replaceWith(
-      `<button type="button" class=" btn btn-danger "><i class="fa-solid fa-plus"></i></button>`
-   );
-   editedValueli.replaceWith(`<input type="text" placeholder="Enter Title"/>`);
-   editback();
+   console.log(localArray)
+   
+   const filterArray = localArray.filter((data) => {
+     return data.state == "2"
+   })
+   console.log(filterArray)
+   filterArray.forEach((element) => {
+      let dynamicList = `<li class ="" id=${element.id}>`;
+      dynamicList += `<div class="wrapper">`;
+      dynamicList += `<input type="range" name="points" data-id=${element.id} min="1" step="1" id="custom-toggle" class="${element.color}" max="3" value="${element.state}">`;
+      dynamicList += `</div>`;
+      dynamicList += `<div class="lable1">`;
+      dynamicList += `<label class="titleLable overflow-hidden">${element.title}</label>`;
+      dynamicList += `</div>`;
+      dynamicList += `<div class= "f-button">`;
+      dynamicList += `<button class="editBtn"><i class="fa-solid fa-pen-to-square"></i></button>`;
+      dynamicList += `<button class="deleteBtn"><i class="fa-solid fa-trash"></i></button>`;
+      dynamicList += `</div>`;
+      dynamicList += `</li>`;
+      $(".todo-li").append(dynamicList);
+   })
 }
+//Complited list
+function Complited(){
+   let val = $(".todo-li").empty()
+   console.log(val,"val")
+   let localdata = localStorage.getItem("localData");
+   let localArray = JSON.parse(localdata);
 
-function editback() {
-   let button2 = $(".todo-inpput").find("button");
-   button2.on("click", function () {
-      AppendElement();
-   });
+   console.log(localArray)
+   
+   const filterArray = localArray.filter((data) => {
+     return data.state == "3"
+   })
+   console.log(filterArray)
+   console.log(filterArray)
+   filterArray.forEach((element) => {
+      let dynamicList = `<li class ="" id=${element.id}>`;
+      dynamicList += `<div class="wrapper">`;
+      dynamicList += `<input type="range" name="points" data-id=${element.id} min="1" step="1" id="custom-toggle" class="${element.color}" max="3" value="${element.state}">`;
+      dynamicList += `</div>`;
+      dynamicList += `<div class="lable1">`;
+      dynamicList += `<label class="titleLable overflow-hidden">${element.title}</label>`;
+      dynamicList += `</div>`;
+      dynamicList += `<div class= "f-button">`;
+      dynamicList += `<button class="editBtn"><i class="fa-solid fa-pen-to-square"></i></button>`;
+      dynamicList += `<button class="deleteBtn"><i class="fa-solid fa-trash"></i></button>`;
+      dynamicList += `</div>`;
+      dynamicList += `</li>`;
+      $(".todo-li").append(dynamicList);
+   })
 }
