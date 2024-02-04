16c450fc8e547bd7b7e27de1e21106cae90e62f0
raj_viradiya_coder <rajviradiya2019@gmial.com>
2024-02-05 00:21:03 +0530

    TODO -v1

--- Main.js
+++ Main.js
@@ -1,41 +1,44 @@
 $(document).ready(function () {
-   loadDataFromLocal();
    startRunningTimersOnLoad();
-   //add loclal storage & update data
+   loadDataFromLocal();
+   /*add loclal storage & update data*/
    $(".todo-inpput").on("click", "button", function () {
       if ($("#txtId").val() == "") {
          addDataToLocal($(this));
          $(".todo-li").empty();
-         loadDataFromLocal()
+         loadDataFromLocal();
       } else {
          updateDataFromLocal();
-         $(".todo-li").empty()
-         loadDataFromLocal()
+         $(".todo-li").empty();
+         loadDataFromLocal();
       }
    });
-
    /**Switch Function */
    $(".todo-li").each(function () {
       $(this).on("click", "#custom-toggle", function () {
          switchElement($(this));
-         $(".todo-li").empty()
-         loadDataFromLocal()
+         $(".todo-li").empty();
+         loadDataFromLocal();
       });
    });
-
    /**Edit element*/
    $(".todo-li").on("click", ".editBtn", function () {
-      const title = $(this).parent(".f-button").prevAll(".lable1").find("label").html();
+      const title = $(this)
+         .parent(".f-button")
+         .prevAll(".lable1")
+         .find("label")
+         .html();
       const id = $(this).parents("li").find("input").attr("data-id");
       $("#input1").val(title);
       $("#txtId").val(id);
-      $(".todo-inpput").find("button").html(`<i class="fa-solid fa-file-export"></i>`);
+      $(".todo-inpput")
+         .find("button")
+         .html(`<i class="fa-solid fa-file-export"></i>`);
    });
-
    /*delete*/
    $(".todo-li").on("click", ".deleteBtn", function () {
       const id = $(this).parents("li").attr("id");
-      const localArray = JSON.parse(localStorage.getItem("localData")) || [];
+      const localArray = JSON.parse(localStorage.getItem("localData"));
       localArray.splice(findIndexById(localArray, parseInt(id)), 1);
       localStorage.setItem("localData", JSON.stringify(localArray));
       function findIndexById(array, id) {
@@ -46,32 +49,34 @@ $(document).ready(function () {
          }
          return -1;
       }
-      $(".todo-li").empty()
-      loadDataFromLocal()
+      $(".todo-li").empty();
+      loadDataFromLocal();
    });
-
    /*clear All*/
    $(".clear").on("click", function () {
       const clearall = $(this).parents(".todo-body").find(".todo-li").empty();
       localStorage.clear();
       loadDataFromLocal();
+      location.reload()
    });
-
    /*filter*/
    $(".btn1").on("click", function () {
-      $(".todo-li").empty()
-      loadDataFromLocal()
-   })
+      $(".todo-li").empty();
+      loadDataFromLocal();
+   });
    $(".btn2").on("click", function () {
-      Active()
-   })
+      Active();
+   });
    $(".btn3").on("click", function () {
-      Complited()
-   })
-
+      Complited();
+   });
    /*Timer*/
    $(".todo-li").on("click", "#custom-toggle", function () {
-      const timerId = $(this).parent(".wrapper").nextAll(".timer").find(".time").attr("data-timer")
+      const timerId = $(this)
+         .parent(".wrapper")
+         .nextAll(".timer")
+         .find(".time")
+         .attr("data-timer");
       if ($(this).val() == 1) {
          resetTimer(timerId);
       } else if ($(this).val() == 2) {
@@ -80,47 +85,49 @@ $(document).ready(function () {
          stopTimer(timerId);
       }
    });
-
 });
 
-/*load html data */
+/*load data */
 function loadDataFromLocal() {
    let localdata = localStorage.getItem("localData");
    let localArray = JSON.parse(localdata);
    if (localdata !== null && localdata !== undefined) {
-      localArray.forEach((element) => {
-         let dynamicList = `<li class ="" id=${element.id}>`;
-         dynamicList += `<div class="wrapper">`;
-         dynamicList += `<input type="range" name="points" data-id=${element.id} min="1" step="1" id="custom-toggle" class="${element.color}" max="3" value="${element.state}">`;
-         dynamicList += `</div>`;
-         dynamicList += `<div class="lable1">`;
-         dynamicList += `<label class="titleLable overflow-hidden">${element.title}</label>`;
-         dynamicList += `</div>`;
-         dynamicList += `<div class="timer">`;
-         dynamicList += `<div class="time"  id="timer${element.id}" data-timer=${element.id}>${element.currentTime}</div>`
-         dynamicList += `</div>`;
-         dynamicList += `<div class= "f-button">`;
-         dynamicList += `<button class="editBtn"><i class="fa-solid fa-pen-to-square"></i></button>`;
-         dynamicList += `<button class="deleteBtn"><i class="fa-solid fa-trash"></i></button>`;
-         dynamicList += `</div>`;
-         dynamicList += `</li>`;
-         $(".todo-li").append(dynamicList);
-         timers.push(createTimer(element.id));
-      })
+      htmlload(localArray)
    }
 }
-
+/**Html */
+function htmlload(localArray) {
+   localArray.forEach((element) => {
+      let dynamicList = `<li class ="" id=${element.id}>`;
+      dynamicList += `<div class="wrapper">`;
+      dynamicList += `<input type="range" name="points" data-id=${element.id} min="1" step="1" id="custom-toggle" class="${element.color}" max="3" value="${element.state}">`;
+      dynamicList += `</div>`;
+      dynamicList += `<div class="lable1">`;
+      dynamicList += `<label class="titleLable overflow-hidden">${element.title}</label>`;
+      dynamicList += `</div>`;
+      dynamicList += `<div class="timer">`;
+      dynamicList += `<div class="time"  id="timer${element.id}" data-timer=${element.id}>${element.currentTime}</div>`;
+      dynamicList += `</div>`;
+      dynamicList += `<div class= "f-button">`;
+      dynamicList += `<button class="editBtn"><i class="fa-solid fa-pen-to-square"></i></button>`;
+      dynamicList += `<button class="deleteBtn"><i class="fa-solid fa-trash"></i></button>`;
+      dynamicList += `</div>`;
+      dynamicList += `</li>`;
+      $(".todo-li").append(dynamicList);
+      timers.push(createTimer(element.id));
+   });
+}
 /*add data to local storage*/
 function addDataToLocal(input1) {
    let localData = localStorage.getItem("localData");
    let input = $(".todo-inpput").find("input");
    let inputval = input.val().trim();
-
    if (localData) {
       let localArray = JSON.parse(localData);
       if (inputval !== "") {
+         let maxId = localArray.reduce((max, obj) => Math.max(max, obj.id), 0);
          const obj = {
-            id: localArray.length + 1,
+            id: maxId + 1,
             state: "1",
             color: "tgl-on",
             starttime: "0:00:00",
@@ -150,42 +157,24 @@ function addDataToLocal(input1) {
       }
    }
 }
-
-
 /**switch function */
 let state = 1;
-
-function switchElement(element) { //#custom-toggle switch = element
+function switchElement(element) {
    let localData = localStorage.getItem("localData");
    let localArray = JSON.parse(localData);
    let value = parseInt(element.val(), 10);
-
-   if (value === 1 && state === 1) {
+   if ((value === 1 && state === 1) || (value === 1 && state === 2) || (value === 2 && state === 1)
+   ) {
       state = 2;
-      element.val(state);
-   } else if (value === 2 && state === 2) {
+   } else if ((value === 2 && state === 2) || (value === 2 && state === 3) || (value === 1 && state === 3) || (value === 3 && state === 2)
+   ) {
       state = 3;
-      element.val(state);
-   } else if (value === 3 && state === 3) {
+   } else if ((value === 3 && state === 3) || (value === 3 && state === 1)) {
       state = 1;
-      element.val(state);
-   } else if (value === 2 && state === 3) {
-      state = 3;
-      element.val(state);
-   } else if (value === 1 && state === 2) {
-      state = 2;
-      element.val(state);
-   } else if (value === 2 && state === 1) {
-      state = 1;
-      element.val(state);
-   } else if (value === 3 && state === 2) {
-      state = 2;
-      element.val(state);
    } else {
       state = 1;
-      element.val(state);
    }
-
+   element.val(state);
    if (state === 1) {
       element.removeClass("tgl-off tgl-def").addClass("tgl-on");
    } else if (state === 2) {
@@ -193,19 +182,16 @@ function switchElement(element) { //#custom-toggle switch = element
    } else if (state === 3) {
       element.removeClass("tgl-def tgl-on").addClass("tgl-off");
    }
-
    localArray.find((data) => {
       if (data.id == element.attr("data-id")) {
          data.state = element.val();
-         data.color = element.attr("class")
+         data.color = element.attr("class");
       }
       localStorage.setItem("localData", JSON.stringify(localArray));
    });
 }
-
 /**Timer */
 const timers = [];
-
 function createTimer(timerId) {
    return {
       id: timerId,
@@ -214,37 +200,33 @@ function createTimer(timerId) {
       elapsedTime: 0,
    };
 }
-
 function getTimer(timerId) {
-   return timers.find(timer => timerId == timer.id);
+   return timers.find((timer) => timerId == timer.id);
 }
-
 function startRunningTimersOnLoad() {
    let localData = JSON.parse(localStorage.getItem("localData"));
    if (localData) {
       localData.forEach((element) => {
          if (element.state == "2") {
             const timer = createTimer(element.id);
-            console.log(element.id,timer)
             timer.elapsedTime = element.elapsedTime || 0;
-            timer.startTime = element.starttime ? moment(element.starttime, 'h:mm:ss') : null;
+            console.log(timer)
+            timer.startTime = element.starttime? moment(element.starttime, "H:mm:ss"): null;
             timer.timerInterval = setInterval(() => updateTimer(timer), 1000);
             timers.push(timer);
          }
       });
    }
 }
-
-
 // start timer
 function startTimer(timerId) {
    let localData = JSON.parse(localStorage.getItem("localData"));
-   let time = moment().format('h:mm:ss')
+   let time = moment().format("H:mm:ss");
    localData.forEach((element) => {
       if (element.id == timerId) {
          element.starttime = time;
       }
-   })
+   });
    localStorage.setItem("localData", JSON.stringify(localData));
    const timer = getTimer(timerId);
    if (!timer.startTime) {
@@ -256,26 +238,29 @@ function startTimer(timerId) {
 function updateTimer(timer) {
    let localData = JSON.parse(localStorage.getItem("localData"));
    const currentTime = moment();
-   const duration = moment.duration(currentTime.diff(timer.startTime) + timer.elapsedTime, 'milliseconds');
-   const formattedTime = moment.utc(duration.asMilliseconds()).format('H:mm:ss');
+   const duration = moment.duration(
+      currentTime.diff(timer.startTime) + timer.elapsedTime,
+      "milliseconds"
+   );
+   const formattedTime = moment.utc(duration.asMilliseconds()).format("H:mm:ss");
    $(`#timer${timer.id}`).text(formattedTime);
 
    localData.forEach((element) => {
       if (element.id == timer.id) {
-         return element.currentTime = formattedTime;
+         element.currentTime = formattedTime
       }
-   })
+   });
    localStorage.setItem("localData", JSON.stringify(localData));
 }
 //stop timer
 function stopTimer(timerId) {
    let localData = JSON.parse(localStorage.getItem("localData"));
-   let time = moment().format("h:mm:ss")
+   let time = moment().format("h:mm:ss");
    localData.forEach((element) => {
       if (element.id == timerId) {
          element.endtime = time;
       }
-   })
+   });
    localStorage.setItem("localData", JSON.stringify(localData));
    const timer = getTimer(timerId);
    if (timer.startTime) {
@@ -290,7 +275,7 @@ function resetTimer(timerId) {
    clearInterval(timer.timerInterval);
    timer.startTime = null;
    timer.elapsedTime = 0;
-   $(`#timer${timer.id}`).text('0:00:00');
+   $(`#timer${timer.id}`).text("0:00:00");
    let localData = JSON.parse(localStorage.getItem("localData"));
    localData.forEach((element) => {
       if (element.id == timerId) {
@@ -301,7 +286,6 @@ function resetTimer(timerId) {
    });
    localStorage.setItem("localData", JSON.stringify(localData));
 }
-
 /**Edit Fuctionality */
 function updateDataFromLocal() {
    let localData = localStorage.getItem("localData");
@@ -313,63 +297,25 @@ function updateDataFromLocal() {
    $("totdo-inpput").find("#input1").val("");
    localStorage.setItem("localData", JSON.stringify(localArray));
 }
-
 //Filter
-
 //Pending list
 function Active() {
-   $(".todo-li").empty()
+   $(".todo-li").empty();
    let localdata = localStorage.getItem("localData");
    let localArray = JSON.parse(localdata);
    const filterArray = localArray.filter((data) => {
-      return data.state == "2"
-   })
-   filterArray.forEach((element) => {
-      let dynamicList = `<li class ="" id=${element.id}>`;
-      dynamicList += `<div class="wrapper">`;
-      dynamicList += `<input type="range" name="points" data-id=${element.id} min="1" step="1" id="custom-toggle" class="${element.color}" max="3" value="${element.state}">`;
-      dynamicList += `</div>`;
-      dynamicList += `<div class="lable1">`;
-      dynamicList += `<label class="titleLable overflow-hidden">${element.title}</label>`;
-      dynamicList += `</div>`;
-      dynamicList += `<div class="timer">`;
-      dynamicList += `<div class="time"  id="timer${element.id}" data-timer=${element.id}>${element.currentTime}</div>`
-      dynamicList += `</div>`;
-      dynamicList += `<div class= "f-button">`;
-      dynamicList += `<button class="editBtn"><i class="fa-solid fa-pen-to-square"></i></button>`;
-      dynamicList += `<button class="deleteBtn"><i class="fa-solid fa-trash"></i></button>`;
-      dynamicList += `</div>`;
-      dynamicList += `</li>`;
-      $(".todo-li").append(dynamicList);
-      timers.push(createTimer(element.id));
-   })
+      return data.state == "2";
+   });
+   htmlload(filterArray)
 }
 //Complited list
 function Complited() {
-   $(".todo-li").empty()
+   $(".todo-li").empty();
    let localdata = localStorage.getItem("localData");
    let localArray = JSON.parse(localdata);
 
    const filterArray = localArray.filter((data) => {
-      return data.state == "3"
-   })
-   filterArray.forEach((element) => {
-      let dynamicList = `<li class ="" id=${element.id}>`;
-      dynamicList += `<div class="wrapper">`;
-      dynamicList += `<input type="range" name="points" data-id=${element.id} min="1" step="1" id="custom-toggle" class="${element.color}" max="3" value="${element.state}">`;
-      dynamicList += `</div>`;
-      dynamicList += `<div class="lable1">`;
-      dynamicList += `<label class="titleLable overflow-hidden">${element.title}</label>`;
-      dynamicList += `</div>`;
-      dynamicList += `<div class="timer">`;
-      dynamicList += `<div class="time"  id="timer${element.id}" data-timer=${element.id}>${element.currentTime}</div>`
-      dynamicList += `</div>`;
-      dynamicList += `<div class= "f-button">`;
-      dynamicList += `<button class="editBtn"><i class="fa-solid fa-pen-to-square"></i></button>`;
-      dynamicList += `<button class="deleteBtn"><i class="fa-solid fa-trash"></i></button>`;
-      dynamicList += `</div>`;
-      dynamicList += `</li>`;
-      $(".todo-li").append(dynamicList);
-      timers.push(createTimer(element.id));
-   })
+      return data.state == "3";
+   });
+   htmlload(filterArray)
 }
